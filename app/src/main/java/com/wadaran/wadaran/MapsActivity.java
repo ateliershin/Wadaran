d36ebe40ae9139768b16fe2494779319436bdd98
atelier.shin@gmail.com <shin0826>
2016-01-07 18:58:40 +0900

    お正月

--- app/src/main/java/com/wadaran/wadaran/MapsActivity.java
+++ app/src/main/java/com/wadaran/wadaran/MapsActivity.java
@@ -55,11 +55,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         myWebView2.getSettings().setJavaScriptEnabled(true);
         myWebView2.loadUrl("http://ghacker.jp/test/superslides-0.6.2/examples/touch.html#1");
 
-        //awaawaaa
-        //aaaaaaaaaaaaaaaaaaaaaa
-        //hogehogehoge
-        //aaaaaaaaaaaa
-        //bbbbbbbbbbbbbbb buhi
     }
 
     public void selectBtn1(View view){
